48204114a5f7c794efff8147a38bf974beaa59e2
divya <hello@shortdiv.com>
2018-08-21 16:55:48 -0500

    maybe this?

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -16,25 +16,29 @@ const db = firebase.database();
 exports.handler = function(event, context, callback) {
   const body = JSON.parse(event.body)
   console.log(body)
-  var newPostKey = db.ref().child(`${this.form.chosenRice}`).push().key;
-  db.ref(`${this.form.chosenRice}/${newPostKey}`).set({
-    ...this.form
-  })
-  .then(() => {
-    console.log('saved')
-    return callback(null, {
-      statusCode: 200,
-      body: "Beep, boop, you just got serverless."
-    })
-  })
-  .catch((err) => {
-    console.log('failed')
-    return callback(null, {
-      statusCode: error.status,
-      body: JSON.stringify({
-        message: error.message,
-        error: error,
-      })
-    })
-  })
+  return callback(null, {
+    statusCode: 200,
+    body: "Hello, World"
+  });
+  // var newPostKey = db.ref().child(`${this.form.chosenRice}`).push().key;
+  // db.ref(`${this.form.chosenRice}/${newPostKey}`).set({
+  //   ...this.form
+  // })
+  // .then(() => {
+  //   console.log('saved')
+  //   return callback(null, {
+  //     statusCode: 200,
+  //     body: "Beep, boop, you just got serverless."
+  //   })
+  // })
+  // .catch((err) => {
+  //   console.log('failed')
+  //   return callback(null, {
+  //     statusCode: error.status,
+  //     body: JSON.stringify({
+  //       message: error.message,
+  //       error: error,
+  //     })
+  //   })
+  // })
 }
