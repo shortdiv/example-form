ebf4d0aca24441eddc4d6eff23f876afb964ac16
divya <hello@shortdiv.com>
2018-08-22 17:30:42 -0500

    firebase

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,14 +1,30 @@
 /* Triggered when a form submission is posted to your site. */
+import firebase from "firebase";
+import "firebase/storage";
+
+var config = {
+  apiKey: `${process.env.VUE_APP_API_KEY}`,
+  authDomain: `${process.env.VUE_APP_PROJECT_ID}.firebaseapp.com`,
+  databaseURL: `https://${process.env.VUE_APP_DB_NAME}.firebaseio.com`,
+  storageBucket: `${process.env.VUE_APP_BUCKET}.appspot.com`,
+  messagingSenderId: `${process.env.VUE_APP_SENDER_ID}`
+};
+
+firebase.initializeApp(config);
+const db = firebase.database();
+
 exports.handler = function(event, context, callback) {
-  console.log('event', JSON.parse(event.body).payload)
-  new Promise((resolve, reject) =>{
-    setTimeout(resolve, 100, context)
+  const body = JSON.parse(event.body).payload
+  var newPostKey = db.ref().child(`${this.form.chosenRice}`).push().key;
+  db.ref(`${this.form.chosenRice}/${newPostKey}`).set({
+    ...body.data,
+    data: body.created_at
   })
-  .then((t) => {
+  .then(() => {
     console.log('saved')
     return callback(null, {
       statusCode: 200,
-      body: JSON.stringify(t)
+      body: "Beep, boop, you just got serverless."
     })
   })
   .catch((err) => {
