a2060394c344620f6b1c0254501a781304254c91
divya <hello@shortdiv.com>
2018-08-22 16:48:08 -0500

    event body

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,7 +1,6 @@
 /* Triggered when a form submission is posted to your site. */
 exports.handler = function(event, context, callback) {
   console.log('event', event)
-  console.log('context', context)
   new Promise((resolve, reject) =>{
     setTimeout(resolve, 100, context)
   })
